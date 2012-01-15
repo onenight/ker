9923ec032461cd6434d131ef0883fa6731b7d667
onenight <artonster7@gmail.com>
2012-01-15 20:47:46 +0800

    New Project start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ker::Application.config.secret_token = '5d4e66b0c0c8dd79f31d510a096a287cb807c6813071c06ddcb1712fc060bf8360abd4d84593be1a967af8f6d56f3c2153991c60aa6ec4ab40cc9de8c7b9aff9'
