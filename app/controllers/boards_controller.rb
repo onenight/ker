b4967f13ea69325b5eee8b44a0207291a5c093b8
onenight <artonster7@gmail.com>
2012-01-16 16:11:51 +0800

    combine board & posts

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -14,6 +14,7 @@ class BoardsController < ApplicationController
   # GET /boards/1.json
   def show
     @board = Board.find(params[:id])
+    @posts = @board.posts
 
     respond_to do |format|
       format.html # show.html.erb
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,54 +1,40 @@
 class PostsController < ApplicationController
+  before_filter :find_board
+  
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render :json => @posts }
-    end
+    redirect_to boards_path
   end
 
   # GET /posts/1
   # GET /posts/1.json
   def show
-    @post = Post.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render :json => @post }
-    end
+    @post = @board.posts.find(params[:id])
   end
 
   # GET /posts/new
   # GET /posts/new.json
   def new
-    @post = Post.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render :json => @post }
-    end
+    @post = @board.posts.build
   end
 
   # GET /posts/1/edit
   def edit
-    @post = Post.find(params[:id])
+    @post = @board.posts.find(params[:id])
   end
 
   # POST /posts
   # POST /posts.json
   def create
-    @post = Post.new(params[:post])
+    @post = @board.posts.build(params[:post])
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to @post, :notice => 'Post was successfully created.' }
-        format.json { render :json => @post, :status => :created, :location => @post }
+        format.html { redirect_to board_post_path(@board, @post), 
+          :notice => 'Post was successfully created.' }
       else
-        format.html { render :action => "new" }
-        format.json { render :json => @post.errors, :status => :unprocessable_entity }
+        format.html { render :new }
       end
     end
   end
@@ -56,15 +42,14 @@ class PostsController < ApplicationController
   # PUT /posts/1
   # PUT /posts/1.json
   def update
-    @post = Post.find(params[:id])
+    @post = @board.posts.find(params[:id])
 
     respond_to do |format|
       if @post.update_attributes(params[:post])
-        format.html { redirect_to @post, :notice => 'Post was successfully updated.' }
-        format.json { head :ok }
+        format.html { redirect_to board_post_path(@board, @post), 
+          :notice => 'Post was successfully updated.' }
       else
-        format.html { render :action => "edit" }
-        format.json { render :json => @post.errors, :status => :unprocessable_entity }
+        format.html { render :edit }
       end
     end
   end
@@ -72,12 +57,16 @@ class PostsController < ApplicationController
   # DELETE /posts/1
   # DELETE /posts/1.json
   def destroy
-    @post = Post.find(params[:id])
+    @post = @board.posts.find(params[:id])
     @post.destroy
 
     respond_to do |format|
-      format.html { redirect_to posts_url }
-      format.json { head :ok }
+      format.html { redirect_to board_posts_path(@board) }
     end
+  end
+  
+  protected
+  def find_board
+    @board = Board.find(params[:board_id])
   end
 end
