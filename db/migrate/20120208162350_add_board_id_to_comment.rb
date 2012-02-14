d91ac8c00fec33588eba8500b51cc2a5bde07866
onenight <artonster7@gmail.com>
2012-02-14 21:41:45 +0800

    comment build

---
+++ db/migrate/20120208162350_add_board_id_to_comment.rb
@@ -0,0 +1,9 @@
+class AddBoardIdToComment < ActiveRecord::Migration
+  def self.up
+    add_column :comments, :board_id, :integer
+  end
+  
+  def self.down
+    remove_column :comments, :board_id
+  end
+end
